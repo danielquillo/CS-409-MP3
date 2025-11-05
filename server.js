0b91c783924da8de3797487c4c229614af667205
danielquillo <dannyq2@illinois.edu>
2025-11-04 23:13:38 -0600

    Updated server.js

--- server.js
+++ server.js
@@ -14,7 +14,8 @@ var app = express();
 var port = process.env.PORT || 3000;
 
 // Connect to a MongoDB --> Uncomment this once you have a connection string!!
-// mongoose.connect(process.env.MONGODB_URI,  { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI,  { useNewUrlParser: true });
+
 
 // Allow CORS so that backend and frontend could be put on different servers
 var allowCrossDomain = function (req, res, next) {
@@ -37,24 +38,5 @@ require('./routes')(app, router);
 app.use(require('./middleware/error'));
 
 // Start the server
-// app.listen(port);
-// console.log('Server running on port ' + port);
-
-const uri = process.env.MONGO_URI || process.env.MONGODB_URI;
-if (!uri) {
-  console.error('Missing MONGO_URI (preferred) or MONGODB_URI in env');
-  process.exit(1);
-}
-
-mongoose.connect(uri, {
-  serverSelectionTimeoutMS: 10000,
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => {
-  app.listen(port, () => console.log('Server running on port ' + port));
-})
-.catch((err) => {
-  console.error('Mongo connection error:', err.message);
-  process.exit(1);
-});
+app.listen(port);
+console.log('Server running on port ' + port);
